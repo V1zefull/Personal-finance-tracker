c3c085439965bc6ed1f256dee0c7412b520c70c1
Borovlev Egor <eab@promtexklg.ru>
2026-07-14 16:19:51 +0300

    feat: setup application

---
+++ src/test/java/com/v1zefull/pft/PftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1zefull.pft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
