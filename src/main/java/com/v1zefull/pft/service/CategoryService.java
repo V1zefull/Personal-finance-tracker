fc0c7e05403101332f21ef202a5c87675d93dda4
Borovlev Egor <eab@promtexklg.ru>
2026-07-31 12:08:01 +0300

    feat: Добавление TransactionService

--- src/main/java/com/v1zefull/pft/service/CategoryService.java
+++ src/main/java/com/v1zefull/pft/service/CategoryService.java
@@ -1,8 +1,11 @@
 package com.v1zefull.pft.service;
 
 import com.v1zefull.pft.entity.Category;
+import com.v1zefull.pft.exception.ResourceNotFoundException;
 import com.v1zefull.pft.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -16,4 +19,18 @@ public class CategoryService {
         return categoryRepository.save(category);
     }
 
+    public Category getCategoryById(Long id){
+        return categoryRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("Category not found with id " + id)
+        );
+    }
+
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public void deleteCategory(Long id){
+        Category category = getCategoryById(id);
+        categoryRepository.deleteById(category.getId());
+    }
 }
